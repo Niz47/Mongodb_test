49a5690902aa3036ee4aa5e73a2be51a9d05ff9e
Niz47 <zinmarhtun47@gmail.com>
2020-03-07 10:23:46 -0800

    increment weight for every student

--- test/update_student.js
+++ test/update_student.js
@@ -8,18 +8,29 @@ describe('Updating student record', function(){
 
     beforeEach(function(done){
         student = new Student({
-            name: "Shaun"
+            name: "Shaun",
+            weight: 20
         });
         student.save().then(function(){
             done();
         });
     });
 
-    // Create test 
+    // Create test 1
     it('Update a record in the database', function(done){
         Student.findOneAndUpdate({name: 'Shaun'}, {name: 'Adam'}).then(function(){
             Student.findOne({_id: student._id}).then(function(result){
                 assert(result.name === 'Adam');
+                done();
+            });
+        });
+    });
+
+    // Create test 2
+    it('Increments the age for every record by 1 in the database', function(done){
+        Student.update({}, {$inc: {weight: 1}}).then(function(){
+            Student.findOne({name: 'Shaun'}).then(function(record){
+                assert(record.weight === 21);
                 done();
             });
         });
