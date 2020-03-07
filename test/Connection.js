c7154c30871a982e2729636ff06b0797d464a29e
Niz47 <zinmarhtun47@gmail.com>
2020-03-07 09:40:26 -0800

    add ES6 Promise

--- test/Connection.js
+++ test/Connection.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
-// Connect to Mongodb
-mongoose.connect('mongodb://localhost/testDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection.once('open', function(){
-    console.log('Connection has been made, now make fireworks...');
-}).on('error', function(error){
-    console.log('Connection Error:' . error);
+// Connect to DB before tests run
+before(function(done){
+    // Connect to Mongodb
+    mongoose.connect('mongodb://localhost/testDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+    mongoose.connection.once('open', function(){
+        console.log('Connection has been made, now make fireworks...');
+        done();
+    }).on('error', function(error){
+        console.log('Connection Error:' . error);
+    });
 });
+
+
