4c57a56906bf0fbdbd4182c2838faa8903f2da03
Niz47 <zinmarhtun47@gmail.com>
2020-03-07 10:01:23 -0800

    find a record by id(object)

--- test/find_student.js
+++ test/find_student.js
@@ -4,8 +4,10 @@ const Student = require('../model/student');
 // Describe test
 describe('Finding student record', function(){
 
+    var student;
+
     beforeEach(function(done){
-        var student = new Student({
+        student = new Student({
             name: "Shaun"
         });
         student.save().then(function(){
@@ -13,10 +15,18 @@ describe('Finding student record', function(){
         });
     });
 
-    // Create tests
-    it('Find record(s) from the database', function(done){
+    // Create test 1
+    it('Find record from the database', function(done){
         Student.findOne({name: 'Shaun'}).then(function(result){
             assert(result.name === 'Shaun');
+            done();
+        });
+    });
+
+    // Create test 2
+    it('Find record by ID from the database', function(done){
+        Student.findOne({_id: student._id}).then(function(result){
+            assert(result._id.toString() === student._id.toString());
             done();
         });
     });
